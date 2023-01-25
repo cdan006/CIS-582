18edde331f2a92fa30c54c8101d4ddabdd44f570
cdan006 <84053642+cdan006@users.noreply.github.com>
2023-01-24 21:16:27 -0500

    Update sign.py

--- sign.py
+++ sign.py
@@ -1,5 +1,5 @@
-from fastecdsa.curve import secp256k1
-from fastecdsa.keys import export_key, gen_keypair
+#from fastecdsa.curve import secp256k1
+#from fastecdsa.keys import export_key, gen_keypair
 
 from fastecdsa import curve, ecdsa, keys, point
 from hashlib import sha256
