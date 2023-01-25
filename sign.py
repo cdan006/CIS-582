08c2d37fbf530c006d1c9007ae90b4ad0b729060
cdan006 <84053642+cdan006@users.noreply.github.com>
2023-01-24 21:20:38 -0500

    Update sign.py

--- sign.py
+++ sign.py
@@ -1,19 +1,15 @@
-#from fastecdsa.curve import secp256k1
-#from fastecdsa.keys import export_key, gen_keypair
+from fastecdsa.curve import secp256k1
+from fastecdsa.keys import export_key, gen_keypair
 
-#from fastecdsa import curve, ecdsa, keys, point
-from fastecdsa import curve
-from fastecdsa import ecdsa
-from fastecdsa import keys
-from fastecdsa import point
+from fastecdsa import curve, ecdsa, keys, point
 from hashlib import sha256
 
 #use the sign and verify functions in https://fastecdsa.readthedocs.io/en/stable/fastecdsa.html
 def sign(m):
 	#generate public key
 	#Your code here
-	private_key = keys.get_private_key(curve = curve.secp256k1)
-	public_key = keys.get_public_key(private_key, curve = curve.secp256k1)
+	private_key = ecdsa.keys.get_private_key(curve = curve.secp256k1)
+	public_key = ecdsa.keys.get_public_key(private_key, curve = curve.secp256k1)
 	r = 0
 	s = 0
 
