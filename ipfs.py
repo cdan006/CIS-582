c32c48382de62f4e0a6878e9a196396ad6514a0d
Chris Daniele <cdan006@seas.upenn.edu>
2023-02-02 17:19:40 -0500

    ipfs

--- ipfs.py
+++ ipfs.py
@@ -1,14 +1,15 @@
 import requests
 import json
 
-
+endpoint = 'https://ipfs.infura.io:5001/api/v0/'
 def pin_to_ipfs(data):
     assert isinstance(data, dict), f"Error pin_to_ipfs expects a dictionary"
     # YOUR CODE HERE
+
     files = {
     'file': json.dumps(data)
     }
-    cid = requests.post('https://ipfs.infura.io:5001/api/v0/add', files=files, auth=(2L9vlTevJDocIJl8kg0wtVXJTC7,ad15ab9acc005b3533f99262f04a6654))
+    cid = requests.post(endpoint+"add", files=files, auth=('2L9vlTevJDocIJl8kg0wtVXJTC7','ad15ab9acc005b3533f99262f04a6654'))
     return cid.text
 
 
@@ -17,7 +18,7 @@ def get_from_ipfs(cid, content_type="json"):
     params = (
         ('arg', cid),
     )
-    data = requests.post('https://ipfs.infura.io:5001/api/v0/cat', params=params,auth=(2L9vlTevJDocIJl8kg0wtVXJTC7, ad15ab9acc005b3533f99262f04a6654))
+    data = requests.post(endpoint+"cat", params=params,auth=('2L9vlTevJDocIJl8kg0wtVXJTC7', 'ad15ab9acc005b3533f99262f04a6654'))
 
     assert isinstance(data, dict), f"get_from_ipfs should return a dict"
     return data
