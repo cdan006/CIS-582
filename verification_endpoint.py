09628140d628000c03597c8b8010f6bbabedcc29
Chris Daniele <cdan006@seas.upenn.edu>
2023-03-15 07:45:30 -0400

    project 1

---
+++ verification_endpoint.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, jsonify
+from flask_restful import Api
+import json
+import eth_account
+import algosdk
+
+app = Flask(__name__)
+api = Api(app)
+app.url_map.strict_slashes = False
+
+@app.route('/verify', methods=['GET','POST'])
+def verify():
+    content = request.get_json(silent=True)
+
+    #Check if signature is valid
+
+    payload = request.json['payload']
+    platform = payload['platform']
+    pk = payload['pk']
+    message = payload['message']
+    sig = request.json['sig']
+    result = False
+    if platform.lower() == 'ethereum':
+        eth_encoded_msg = eth_account.messages.encode_defunct(text=json.dumps(payload))
+        if eth_account.Account.recover_message(eth_encoded_msg, signature=sig) == pk:
+            result= jsonify(True)
+        else:
+            result= jsonify(False)
+    elif platform.lower() == 'algorand':
+        if algosdk.util.verify_bytes(json.dumps(payload).encode('utf-8'), sig, pk):
+            result= jsonify(True)
+        else:
+            result= jsonify(False)
+
+    return result
+
+if __name__ == '__main__':
+    app.run(port='5002')
