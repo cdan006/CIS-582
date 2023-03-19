b54f5f6955ce5945eed70de69ee65369916b03f5
Chris Daniele <cdan006@seas.upenn.edu>
2023-03-19 12:34:25 -0400

    amm updated4

--- AMM.sol
+++ AMM.sol
@@ -60,20 +60,21 @@ contract AMM is AccessControl{
 		//YOUR CODE HERE
 		if (sellToken == tokenA) {
 		    qtyA = (ERC20(tokenA).balanceOf(address(this))) + sellAmount;
-		    //exchangeRate = (qtyA / (ERC20(tokenB).balanceOf(address(this))));
-		    exchangeRate = (((ERC20(tokenA).balanceOf(address(this)))+sellAmount) / (ERC20(tokenB).balanceOf(address(this))));
-		    qtyB = (ERC20(tokenB).balanceOf(address(this)))/exchangeRate;
-		    swapAmt = qtyB*((10000 - feebps)/10000);
+		    //exchangeRate = qtyA/ (ERC20(tokenB).balanceOf(address(this))));
+		    //qtyB = (ERC20(tokenB).balanceOf(address(this)))/exchangeRate;
+		    qtyB = (10000 + feebps)*invariant/qtyA*10000;
+		    //swapAmt = qtyB*((10000 - feebps)/10000);
+		    swapAmt = (ERC20(tokenB).balanceOf(address(this))) - qtyB;
 			ERC20(tokenA).transferFrom(msg.sender, address(this), sellAmount);
 			ERC20(tokenB).transfer(msg.sender, swapAmt);
 		}
 
 		if (sellToken == tokenB){
 		    qtyB = (ERC20(tokenB).balanceOf(address(this))) + sellAmount;
-		    //exchangeRate = (qtyB / (ERC20(tokenA).balanceOf(address(this))));
-		    exchangeRate = (((ERC20(tokenB).balanceOf(address(this)))+sellAmount)  / (ERC20(tokenA).balanceOf(address(this))));
-		    qtyA = (ERC20(tokenA).balanceOf(address(this)))/exchangeRate;
-		    swapAmt = qtyA*((10000 - feebps)/10000);
+		    //exchangeRate =   qtyB/ (ERC20(tokenA).balanceOf(address(this))));
+		    //qtyA = (ERC20(tokenA).balanceOf(address(this)))/exchangeRate;
+		    qtyA = (10000 + feebps)*invariant/qtyB*10000;
+		    swapAmt = (ERC20(tokenA).balanceOf(address(this))) - qtyA;
 			ERC20(tokenB).transferFrom(msg.sender, address(this), sellAmount);
 			ERC20(tokenA).transfer(msg.sender, swapAmt);
 		}
@@ -93,6 +94,7 @@ contract AMM is AccessControl{
 		//YOUR CODE HERE
 		ERC20(tokenA).transferFrom(msg.sender, address(this), amtA);
 		ERC20(tokenB).transferFrom(msg.sender, address(this), amtB);
+        invariant = ERC20(tokenA).balanceOf(address(this))*ERC20(tokenB).balanceOf(address(this));
 
 		emit LiquidityProvision( msg.sender, amtA, amtB );
 	}
