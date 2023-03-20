867aa4912b1541977a671c35fec6728212edf5ba
Chris Daniele <cdan006@seas.upenn.edu>
2023-03-20 09:07:30 -0400

    send tokens

---
+++ send_tokens.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+
+from algosdk.v2client import algod
+from algosdk import mnemonic
+from algosdk import transaction
+
+#Connect to Algorand node maintained by PureStake
+algod_address = "https://testnet-algorand.api.purestake.io/ps2"
+algod_token = "B3SU4KcVKi94Jap2VXkK83xx38bsv95K5UZm2lab"
+#algod_token = 'IwMysN3FSZ8zGVaQnoUIJ9RXolbQ5nRY62JRqF2H'
+headers = {
+   "X-API-Key": algod_token,
+}
+
+acl = algod.AlgodClient(algod_token, algod_address, headers)
+min_balance = 100000 #https://developer.algorand.org/docs/features/accounts/#minimum-balance
+
+def send_tokens( receiver_pk, tx_amount ):
+    params = acl.suggested_params()
+    gen_hash = params.gh
+    first_valid_round = params.first
+    tx_fee = params.min_fee
+    last_valid_round = params.last
+
+    #Your code here
+    mnemonic_s = "original crisp season bike anchor live punch survey reject egg wink moon obvious paddle hazard engage elephant chunk panther violin daughter hen genre ability alarm"
+    sender_sk = mnemonic.to_private_key(mnemonic_s)
+    sender_pk = mnemonic.to_public_key(mnemonic_s)
+    txn = transaction.PaymentTxn(sender_pk, tx_fee, first_valid_round, last_valid_round, gen_hash, receiver_pk, tx_amount)
+    signed_txn = txn.sign(sender_sk)
+    txid = acl.send_transaction(signed_txn)
+    wait_for_confirmation(acl, txid)
+
+    return sender_pk, txid
+
+# Function from Algorand Inc.
+def wait_for_confirmation(client, txid):
+    """
+    Utility function to wait until the transaction is
+    confirmed before proceeding.
+    """
+    last_round = client.status().get('last-round')
+    txinfo = client.pending_transaction_info(txid)
+    while not (txinfo.get('confirmed-round') and txinfo.get('confirmed-round') > 0):
+        print("Waiting for confirmation")
+        last_round += 1
+        client.status_after_block(last_round)
+        txinfo = client.pending_transaction_info(txid)
+    print("Transaction {} confirmed in round {}.".format(txid, txinfo.get('confirmed-round')))
+    return txinfo
+
