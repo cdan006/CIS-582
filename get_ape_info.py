0ae365baa77ebb3722a6e17cad0b11c561f7fc48
Chris Daniele <cdan006@seas.upenn.edu>
2023-03-27 20:42:09 -0400

    amm

--- get_ape_info.py
+++ get_ape_info.py
@@ -1,10 +1,10 @@
+from sqlalchemy import null
 from web3 import Web3
 from web3.contract import Contract
 from web3.providers.rpc import HTTPProvider
 import requests
 import json
 import time
-from  bayc_supply import api_url
 
 bayc_address = "0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D"
 contract_address = Web3.toChecksumAddress(bayc_address)
@@ -21,7 +21,8 @@ with open('/home/codio/workspace/abi.json', 'r') as f:
 api_url = "https://api.etherscan.io/api?module=contract&action=getabi&address=0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D"# YOU WILL NEED TO TO PROVIDE THE URL OF AN ETHEREUM NODE
 provider = HTTPProvider(api_url)
 web3 = Web3(provider)
-
+cid = "QmeSjSinHpPnmXmspMjwiXyN6zS4E9zccariGR3jxcaWtq/1"
+gateway = {'infura': "https://ipfs.infura.io:5001/api/v0/cat?arg=QmeSjSinHpPnmXmspMjwiXyN6zS4E9zccariGR3jxcaWtq/1", 'pinata': "https://gateway.pinata.cloud/ipfs/QmeSjSinHpPnmXmspMjwiXyN6zS4E9zccariGR3jxcaWtq/1", 'ipfs': "https://ipfs.io/ipfs/QmeSjSinHpPnmXmspMjwiXyN6zS4E9zccariGR3jxcaWtq/1"}
 
 def get_ape_info(apeID):
     assert isinstance(apeID, int), f"{apeID} is not an int"
@@ -33,11 +34,22 @@ def get_ape_info(apeID):
     contract = web3.eth.contract(address=contract_address, abi=abi)
     data['owner'] = contract.functions.ownerOf(apeID).call()
     token_uri = contract.functions.tokenURI
-    gateway_url = 'https://gateway.pinata.cloud/ipfs/'
-    ipfs_hash = token_uri.replace('ipfs://', '')
-    metadata_url = gateway_url + ipfs_hash
-    response = requests.get(metadata_url)
-    metadata = response.json()
+    #gateway_url = 'https://gateway.pinata.cloud/ipfs/'
+    #ipfs_hash = token_uri.replace('ipfs://', '')
+    #metadata_url = gateway_url + ipfs_hash
+    """
+    for x in gateway:
+        if gateway.keys()!='infura':
+            response = requests.get(x.values()+cid) #convert this to string.  Then convert to Json
+            metadata = response.json()
+        else:
+            response = requests.post(x.values() + cid)  # convert this to string.  Then convert to Json
+            metadata = response.json()
+        if metadata is not null:
+            break
+        """
+    response = requests.get(gateway['pinata'].values() + cid).text  # convert this to string.  Then convert to Json
+    metadata = json.loads(response.json())
     data['image'] = metadata['image']
     attributes = metadata['attributes']
     for a in attributes:
@@ -51,3 +63,6 @@ def get_ape_info(apeID):
                 ['owner', 'image', 'eyes']]), f"return value should include the keys 'owner','image' and 'eyes'"
     return data
 
+if __name__ == "__main__":
+    data = get_ape_info(1)
+    print(data['owner'])
