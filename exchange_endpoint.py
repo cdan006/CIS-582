3372f10ad216c2c183f664fd82d0aab373e06802
Chris Daniele <cdan006@seas.upenn.edu>
2023-04-16 15:24:47 -0400

    project V

--- exchange_endpoint.py
+++ exchange_endpoint.py
@@ -390,8 +390,8 @@ def trade():
                 print("sell_amount", new_order.sell_amount)
                 print("sell_amount type", type(new_order.sell_amount))
                 if transaction_amount == new_order.sell_amount:
-                    print("equal_sell_amount", equal_sell_amount)
                     equal_sell_amount = True
+                    print("equal_sell_amount", equal_sell_amount)
             elif platform == "Ethereum":
                 transactions = g.w3.eth.getBalance(new_order.sender_pk)
                 #transactions = w3.eth.get_transaction(new_order.tx_id)
@@ -405,6 +405,7 @@ def trade():
             g.session.commit()
             print("test")
             fill_order(new_order)
+            print("test2")
             result = jsonify(True)
             print("Returning jsonify(True) as everything went well")
             return result
