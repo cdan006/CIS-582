90516dcc79ed40e0e7cca0a2bdbf56bd1bc8af0f
Chris Daniele <cdan006@seas.upenn.edu>
2023-04-16 08:20:45 -0400

    project V

--- exchange_endpoint.py
+++ exchange_endpoint.py
@@ -338,7 +338,7 @@ def trade():
     print("In trade", file=sys.stderr)
     print("In trade test")
     connect_to_blockchains()
-    get_keys()
+    #get_keys()
     if request.method == "POST":
         content = request.get_json(silent=True)
         columns = ["buy_currency", "sell_currency", "buy_amount", "sell_amount", "platform", "tx_id", "receiver_pk"]
@@ -360,6 +360,7 @@ def trade():
         if error:
             print(json.dumps(content))
             return jsonify(False)
+
 
         # Your code here
 
