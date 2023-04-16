db78790a74cb060461c6b7981c9ebe8b70fe32fd
Chris Daniele <cdan006@seas.upenn.edu>
2023-04-16 12:43:14 -0400

    project V

--- exchange_endpoint.py
+++ exchange_endpoint.py
@@ -382,6 +382,7 @@ def trade():
 
             if platform == "Algorand":
                 transactions = g.icl.search_transactions(txid=new_order.tx_id)
+                print("transactions", transactions)
                 for x in transactions:
                     print("x",x)
                     if x['sell_amount'] == new_order.sell_amount:
