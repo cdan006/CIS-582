6e7dd8e22b2e356fdd961e491b0cb5ec99e5d977
Chris Daniele <cdan006@seas.upenn.edu>
2023-04-17 07:00:09 -0400

    project V

--- exchange_endpoint.py
+++ exchange_endpoint.py
@@ -171,11 +171,11 @@ def fill_order(order, txes=[]):
     print("2")
     print("orders_iterate", orders_iterate)
     orders_iterate_removedNone = g.session.query(Order).filter(
-        #Order.filled.is_(None),
+        Order.filled.is_(None),
         Order.buy_currency == new_order.sell_currency,
-        Order.sell_currency == new_order.buy_currency,
+        #Order.sell_currency == new_order.buy_currency,
         ((Order.sell_amount / Order.buy_amount) >= (new_order.buy_amount / new_order.sell_amount)),
-        #((Order.sell_amount * new_order.sell_amount) >= (Order.buy_amount * new_order.buy_amount)),
+        ((Order.sell_amount * new_order.sell_amount) >= (Order.buy_amount * new_order.buy_amount)),
     ).all()
     print("orders_iterate_removedNone", orders_iterate_removedNone)
     orders_iterate_removedbc = g.session.query(Order).filter(
